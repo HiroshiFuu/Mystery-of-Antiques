abf67844bb153d5874fcc21addc6beae0b898f7c
Feng Hao <hiroushifuu@outlook.com>
2020-11-14 01:21:40 +0800

    feat: implement setup game
    -TODO: game

--- apps/Helper/urls.py
+++ apps/Helper/urls.py
@@ -1,10 +1,15 @@
-from django.conf.urls import url
+# -*- encoding: utf-8 -*-
+
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'Helper'
 
 urlpatterns = [
-    url(r'^Home/$', views.Home, name='Home'),
-    url(r'^CreateGame/$', views.CreateGame, name='CreateGame'),
+    path('home/', views.home, name='home'),
+    path('create_game/', views.create_game, name='create_game'),
+    path('setup_game/<int:room_id>/', views.setup_game, name='setup_game'),
+    path('complete_setup_game/<int:room_id>/', views.complete_setup_game, name='complete_setup_game'),
+    path('game/<int:room_id>/', views.game, name='game'),
 ]
--- apps/Helper/views.py
+++ apps/Helper/views.py
@@ -12,12 +12,12 @@ import random
 import json
 
 # Create your views here.
-def Home(request):
+def home(request):
 	return render(request, 'home.html', {})
 
 
-def CreateGame(request):
-	print('CreateGame')
+def create_game(request):
+	print('create_game')
 	room_id = random.randint(10000, 32767)
 	while Game.objects.filter(room_id=room_id).count() > 0:
 		room_id = random.randint(10000, 32767)
@@ -38,8 +38,68 @@ def CreateGame(request):
 		new_game.zodiacs.add(zodiac)
 		zodiac_names.remove(name)
 		zodiac_genuines.remove(genuine)
-	request.session['create_at'] = str(timezone.now())
-	characters = Character.objects.all()
-	for character in characters:
-		new_game.characters.add(character)
 	return render(request, 'to_setup.html', {'room_id': room_id})
+
+
+def setup_game(request, room_id):
+	# room_id = request.POST.get('room_id', None)
+	if room_id is None or len(str(room_id)) != 5:
+		return redirect('Helper:home')
+	game = Game.objects.filter(room_id=room_id).first()
+	if game is None:
+		return redirect('Helper:home')
+	# print('setup_game')
+	characters = []
+	for character in CHARACTOR_CHOICES:
+		characters.append({'value': character[0], 'name': character[1]})
+	return render(request, 'setup.html', {'room_id': room_id, 'numbers_of_players': range(1, 9), 'characters': characters})
+
+
+def complete_setup_game(request, room_id):
+	# room_id = request.POST.get('room_id', None)
+	if room_id is None or len(str(room_id)) != 5:
+		return redirect('Helper:home')
+	game = Game.objects.filter(room_id=room_id).first()
+	if game is None:
+		return redirect('Helper:home')
+	print('complete_setup_game')
+	players = []
+	game.players.all().delete()
+	for i in range(1, 9):
+		player_name = request.POST.get('player_name' + str(i), None)
+		character_name = request.POST.get('character_name' + str(i), None)
+		player_code = random.randint(100, 999)
+		while Player.objects.filter(player_code=player_code).count() > 0:
+			player_code = random.randint(100, 999)
+		character = Character.objects.get(name=character_name)
+		player, created = Player.objects.get_or_create(name=player_name, player_code=player_code, character=character, game=game)
+		players.append({'player_name': player_name, 'character_name': character_name, 'player': player})
+		game.players.add(player)
+	print(players)
+	print(game)
+	return render(request, 'to_game.html', {'room_id': room_id})
+
+
+def game(request):
+	room_id = request.POST.get('room_id', None)
+	if room_id is None:
+		return redirect('Helper:home')
+
+	game = Game.objects.get(room_id=room_id)
+	if game.stage == -1:
+		# game.stage = 0
+		# To-Do: another ready check to set to 1
+		game.stage = 101
+		# game.save()
+		# all_colors = get_all_colors()
+		# start_color = all_colors[game.start_color_index]
+		# player = game.players.filter(color=color).first()
+		player = game.players.all().first() # for testing
+		# player.sequence = 1
+		# player.save()
+	player_code = request.session.get('player_code', None)
+	me = Player.objects.get(player_code=player_code)
+	players = game.players.all()
+	return HttpResponse(players)
+	# return render(request, 'game.html', {'game': game, 'me': me, 'room_id': room_id, 'players': players})
+
